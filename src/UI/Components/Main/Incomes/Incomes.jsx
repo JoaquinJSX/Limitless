f8a3a53aabcb7def475ffa375d53fe13cb445c82
Joaquin Palacios <joaquinpalaciosgomez04@gmail.com>
2024-11-10 16:32:19 -0500

    upgrating Income table

--- src/UI/Components/Main/Incomes/Incomes.jsx
+++ src/UI/Components/Main/Incomes/Incomes.jsx
@@ -4,6 +4,23 @@ import './Incomes.css';
 export default function Incomes({ incomes, setIncomes }) {
 
     const [isShowingExpenses, setIsShowingExpenses] = useState(true);
+    const [newIncome, setNewIncome] = useState({
+        day: null,
+        amount: null,
+        currency: null,
+        source: null
+    });
+
+    function padStart(number) {
+        return (number < 10 ? '0' : '') + number;
+    }
+
+    function addNewIncome() {
+        if (newIncome.day && newIncome.amount && newIncome.currency && newIncome.source) {
+            setIncomes(i => [...i, newIncome]);
+            setIsShowingExpenses(true);
+        }
+    }
 
     return (
         <div className='incomes'>
@@ -16,17 +33,23 @@ export default function Incomes({ incomes, setIncomes }) {
                             <th>Currency</th>
                             <th>Source of money</th>
                         </tr>
-                        <tr style={{ backgroundColor: "lightgrey", color: "black" }}>
-                            <td>{incomes[0].day}</td>
-                            <td>1324.50</td>
-                            <td>Soles</td>
-                            <td>Salary</td>
-                        </tr>
+                        {incomes.map((income, index) =>
+                            <tr key={index} style={{ backgroundColor: "lightgrey", color: "black" }}>
+                                <td>{income.day}</td>
+                                <td>{income.amount}</td>
+                                <td>{income.currency}</td>
+                                <td>{income.source}</td>
+                            </tr>)}
                     </table>
-                    <button className='add' onClick={() => setIsShowingExpenses(false)}>+</button>
+                    <button
+                        className='add'
+                        onClick={() => setIsShowingExpenses(false)}
+                    >
+                        +
+                    </button>
                 </div> :
-                <div>
-                    <h3 style={{color: 'white'}}>Create new income</h3>
+                <div className='add-income'>
+                    <h3 style={{ color: 'white' }}>Create new income</h3>
                     <table>
                         <tr style={{ backgroundColor: "gray", color: "black" }}>
                             <th>Day</th>
@@ -35,14 +58,51 @@ export default function Incomes({ incomes, setIncomes }) {
                             <th>Source of money</th>
                         </tr>
                         <tr style={{ backgroundColor: "lightgrey", color: "black" }}>
-                            <td><input type="text" /></td>
-                            <td><input type="number" /></td>
-                            <td><input type="text" /></td>
-                            <td><input type="text" /></td>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder='Insert data'
+                                    value={newIncome.day}
+                                    onChange={e => setNewIncome(i => ({ ...i, day: e.target.value }))}
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    placeholder='Insert data'
+                                    onChange={e => setNewIncome(i => ({ ...i, amount: e.target.value }))}
+                                />
+                            </td>
+                            <td>
+                                <select
+                                    name="currency"
+                                    onChange={e => setNewIncome(i => ({ ...i, currency: e.target.value }))}
+                                >
+                                    <option value="Soles">Soles</option>
+                                    <option value="Dolars">Dolars</option>
+                                </select>
+                            </td>
+                            <td>
+                                <select
+                                    name="sourceOfMoney"
+                                    onChange={e => setNewIncome(i => ({ ...i, source: e.target.value }))}
+                                >
+                                    <option value="Launch">Launch</option>
+                                    <option value="Drugs">Drugs</option>
+                                    <option value="Bitchs">Bitchs</option>
+                                    <option value="Others">Others</option>
+                                </select>
+                            </td>
                         </tr>
                     </table>
-                    <button className='add'>Cancel</button>
-                    <button>Save</button>
+                    <button
+                        className='cancel'
+                        onClick={() => setIsShowingExpenses(true)}
+                    >
+                        Cancel
+                    </button>
+                    <button onClick={addNewIncome}>Save</button>
+                    <p style={{ color: 'white' }}>{newIncome.currency}</p>
                 </div>}
         </div>
     );
